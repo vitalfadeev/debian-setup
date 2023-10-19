78cb9c6cda438910fb32b2fce393c4a95a86fd56
Vital Fadeev <vital.fadeev@gmail.com>
2023-10-19 15:19:00 +0700

    upd

---
+++ src/etc/console-setup/cached_setup_keyboard.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if [ -f /run/console-setup/keymap_loaded ]; then
+    rm /run/console-setup/keymap_loaded
+    exit 0
+fi
+# logind.conf
+# [Login]
+# NAutoVTs=2
+# ReserveVT=2
+kbd_mode '-u' < '/dev/tty1' 
+kbd_mode '-u' < '/dev/tty2' 
+#kbd_mode '-u' < '/dev/tty3' 
+#kbd_mode '-u' < '/dev/tty4' 
+#kbd_mode '-u' < '/dev/tty5' 
+#kbd_mode '-u' < '/dev/tty6' 
+loadkeys '/etc/console-setup/cached_UTF-8_del.kmap.gz' > '/dev/null' 
